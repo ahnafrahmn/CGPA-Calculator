dc1fada88c55f373c10d36ed285d2a9e5e13a7ed
Ahnaf <107554314+ahnafrahmn@users.noreply.github.com>
2022-08-11 14:04:41 +0600

    Update CGPA-Calculator.py

--- CGPA-Calculator.py
+++ CGPA-Calculator.py
@@ -1,344 +1,64 @@
-print(" CGPA CALCULATOR ")
 
 
-name = input("Enter your name :: ")
+name = input("Enter your name. \n  ::  ")
 
-print("You have to give space between grade and credit hours ( Example: B+ 4 or b+ 4 )")
+courseno = None
+while courseno == None:
+    try:
+        courseno = int(input("how many courses ? \n  ::  "))
+    except:
+        print("Error, Enter a number! ")
 
+result = 0
+creditHrs = 0
+allCH = 0
+i = 0
+while i < courseno:
+    grade = input(
+        "Enter your grade in letter for the %s no. course ::  " % (str(i+1)))
+    creditHrs = int(input("Enter your credit hours for this course  ::  "))
+    grade = grade.upper()
+    if grade == "A" or grade == "B+" or grade == "B" or grade == "B-" or grade == "C+" or grade == "C" or grade == "C-" or grade == "D+" or grade == "D" or grade == "F":
+        i += 1
+        allCH += creditHrs
+        if grade == "A":
+            result += 4*creditHrs
 
+        elif grade == "B+":
+            result += 3.7*creditHrs
 
+        elif grade == "B":
+            result += 3.4*creditHrs
 
-while True:
-    c1g, c1ch = input("Enter grade & credit hours of 1st course :: ").split()
-    if (c1g == "A") or (c1g == "a"):
-        c1g = 4
-        break
-    elif (c1g == "B+") or (c1g == "b+"):
-        c1g = 3.7
-        break    
-    elif (c1g == "B") or (c1g == "b"):
-        c1g = 3.4
-        break    
-    elif (c1g == "B-") or (c1g == "b-"):
-        c1g = 3.1 
-        break   
-    elif (c1g == "C+") or (c1g == "c+"):
-        c1g = 2.8    
-        break
-    elif (c1g == "C") or (c1g == "c"):
-        c1g = 2.5    
-        break
-    elif (c1g == "C-") or (c1g == "c-"):
-        c1g = 2.2
-        break  
-    elif (c1g == "D+") or (c1g == "d+"):
-        c1g = 1.5  
-        break  
-    elif (c1g == "D") or (c1g == "d"):
-        c1g = 1.0
-        break 
-    elif (c1g == "F") or (c1g == "f"):
-        c1g = 0.0
-        break
-            
+        elif grade == "B-":
+            result += 3.1*creditHrs
+
+        elif grade == "C+":
+            result += 2.8*creditHrs
+
+        elif grade == "C":
+            result += 2.5*creditHrs
+
+        elif grade == "C-":
+            result += 2.2*creditHrs
+
+        elif grade == "D+":
+            result += 1.5*creditHrs
+
+        elif grade == "D":
+            result += 1.0*creditHrs
+
+        elif grade == "F":
+            result += 0.0*creditHrs
+
     else:
-        print("Invalid grade. please try again.")
-        continue
+        print("invalid grade!!")
 
 
+creditHrs = float(creditHrs)
+result = float(result)
+allCH = float(allCH)
 
 
-
-
-while True:
-    c2g, c2ch = input("Enter grade & credit hours of 2nd course :: ").split()
-    if (c2g == "A") or (c2g == "a"):
-        c2g = 4
-        break
-    elif (c2g == "B+") or (c2g == "b+"):
-        c2g = 3.7
-        break    
-    elif (c2g == "B") or (c2g == "b"):
-        c2g = 3.4
-        break    
-    elif (c2g == "B-") or (c2g == "b-"):
-        c2g = 3.1 
-        break   
-    elif (c2g == "C+") or (c2g == "c+"):
-        c2g = 2.8    
-        break
-    elif (c2g == "C") or (c2g == "c"):
-        c2g = 2.5    
-        break
-    elif (c2g == "C-") or (c2g == "c-"):
-        c2g = 2.2
-        break  
-    elif (c2g == "D+") or (c2g == "d+"):
-        c2g = 1.5  
-        break  
-    elif (c2g == "D") or (c2g == "d"):
-        c2g = 1.0
-        break 
-    elif (c2g == "F") or (c2g == "f"):
-        c2g = 0.0
-        break
-    elif (c2g == "0") or (c2ch == "0"):
-        c2g = 0.0 ; c2ch = 0.0
-        break
-            
-    else:
-        print("Invalid grade. please try again.")
-        continue
-
-
-
-
-
-
-while True:
-    c3g, c3ch = input("Enter grade & credit hours of 3rd course :: ").split()
-    if (c3g == "A") or (c3g == "a"):
-        c3g = 4
-        break
-    elif (c3g == "B+") or (c3g == "b+"):
-        c3g = 3.7
-        break    
-    elif (c3g == "B") or (c3g == "b"):
-        c3g = 3.4
-        break    
-    elif (c3g == "B-") or (c3g == "b-"):
-        c3g = 3.1 
-        break   
-    elif (c3g == "C+") or (c3g == "c+"):
-        c3g = 2.8    
-        break
-    elif (c3g == "C") or (c3g == "c"):
-        c3g = 2.5    
-        break
-    elif (c3g == "C-") or (c3g == "c-"):
-        c3g = 2.2
-        break  
-    elif (c3g == "D+") or (c3g == "d+"):
-        c3g = 1.5  
-        break  
-    elif (c3g == "D") or (c3g == "d"):
-        c3g = 1.0
-        break 
-    elif (c3g == "F") or (c3g == "f"):
-        c3g = 0.0
-        break
-    elif (c3g == "0") or (c3ch == "0"):
-        c3g = 0.0 ; c3ch = 0.0
-        break
-            
-    else:
-        print("Invalid grade. please try again.")
-        continue
-
-
-
-
-while True:
-    c4g, c4ch = input("Enter grade & credit hours of 4th course :: ").split()
-    if (c4g == "A") or (c4g == "a"):
-        c4g = 4
-        break
-    elif (c4g == "B+") or (c4g == "b+"):
-        c4g = 3.7
-        break    
-    elif (c4g == "B") or (c4g == "b"):
-        c4g = 3.4
-        break    
-    elif (c4g == "B-") or (c4g == "b-"):
-        c4g = 3.1 
-        break   
-    elif (c4g == "C+") or (c4g == "c+"):
-        c4g = 2.8    
-        break
-    elif (c4g == "C") or (c4g == "c"):
-        c4g = 2.5    
-        break
-    elif (c4g == "C-") or (c4g == "c-"):
-        c4g = 2.2
-        break  
-    elif (c4g == "D+") or (c4g == "d+"):
-        c4g = 1.5  
-        break  
-    elif (c4g == "D") or (c4g == "d"):
-        c4g = 1.0
-        break 
-    elif (c4g == "F") or (c4g == "f"):
-        c4g = 0.0
-        break
-    elif (c4g == "0") or (c4ch == "0"):
-        c4g = 0.0 ; c4ch = 0.0
-        break
-            
-    else:
-        print("Invalid grade. please try again.")
-        continue
-
-
-
-
-
-
-while True:
-    c5g, c5ch = input("Enter grade & credit hours of 5th course :: ").split()
-    if (c5g == "A") or (c5g == "a"):
-        c2g = 4
-        break
-    elif (c5g == "B+") or (c5g == "b+"):
-        c5g = 3.7
-        break    
-    elif (c5g == "B") or (c5g == "b"):
-        c5g = 3.4
-        break    
-    elif (c5g == "B-") or (c5g == "b-"):
-        c5g = 3.1 
-        break   
-    elif (c5g == "C+") or (c5g == "c+"):
-        c5g = 2.8    
-        break
-    elif (c5g == "C") or (c5g == "c"):
-        c5g = 2.5    
-        break
-    elif (c5g == "C-") or (c5g == "c-"):
-        c5g = 2.2
-        break  
-    elif (c5g == "D+") or (c5g == "d+"):
-        c5g = 1.5  
-        break  
-    elif (c5g == "D") or (c5g == "d"):
-        c5g = 1.0
-        break 
-    elif (c5g == "F") or (c5g == "f"):
-        c5g = 0.0
-        break
-    elif (c5g == "0") or (c5ch == "0"):
-        c5g = 0.0 ; c5ch = 0.0
-        break
-            
-    else:
-        print("Invalid grade. please try again.")
-        continue
-
-
-
-
-while True:
-    c6g, c6ch = input("Enter grade & credit hours of 6th course :: ").split()
-    if (c6g == "A") or (c6g == "a"):
-        c6g = 4
-        break
-    elif (c6g == "B+") or (c6g == "b+"):
-        c6g = 3.7
-        break    
-    elif (c6g == "B") or (c6g == "b"):
-        c6g = 3.4
-        break    
-    elif (c6g == "B-") or (c6g == "b-"):
-        c6g = 3.1 
-        break   
-    elif (c6g == "C+") or (c6g == "c+"):
-        c6g = 2.8    
-        break
-    elif (c6g == "C") or (c6g == "c"):
-        c6g = 2.5    
-        break
-    elif (c6g == "C-") or (c6g == "c-"):
-        c6g = 2.2
-        break  
-    elif (c6g == "D+") or (c6g == "d+"):
-        c6g = 1.5  
-        break  
-    elif (c6g == "D") or (c6g == "d"):
-        c6g = 1.0
-        break 
-    elif (c6g == "F") or (c6g == "f"):
-        c6g = 0.0
-        break
-    elif (c6g == "0") or (c6ch == "0"):
-        c6g = 0.0 ; c6ch = 0.0
-        break
-            
-    else:
-        print("Invalid grade. please try again.")
-        continue
-
-
-
-
-
-
-while True:
-    c7g, c7ch = input("Enter grade & credit hours of 7th course :: ").split()
-    if (c7g == "A") or (c7g == "a"):
-        c7g = 4
-        break
-    elif (c7g == "B+") or (c7g == "b+"):
-        c7g = 3.7
-        break    
-    elif (c7g == "B") or (c7g == "b"):
-        c7g = 3.4
-        break    
-    elif (c7g == "B-") or (c7g == "b-"):
-        c7g = 3.1 
-        break   
-    elif (c7g == "C+") or (c7g == "c+"):
-        c7g = 2.8    
-        break
-    elif (c7g == "C") or (c7g == "c"):
-        c7g = 2.5    
-        break
-    elif (c7g == "C-") or (c7g == "c-"):
-        c7g = 2.2
-        break  
-    elif (c7g == "D+") or (c7g == "d+"):
-        c7g = 1.5  
-        break  
-    elif (c7g == "D") or (c7g == "d"):
-        c7g = 1.0
-        break 
-    elif (c7g == "F") or (c7g == "f"):
-        c7g = 0.0
-        break
-    elif (c7g == "0") or (c7ch == "0"):
-        c7g = 0.0 ; c7ch = 0.0
-        break
-            
-    else:
-        print("Invalid grade. please try again.")
-        continue
-
-
-
-c1g = float(c1g)
-c1ch = float(c1ch)
-c2g = float(c2g)
-c2ch = float(c2ch)
-c3g = float(c3g)
-c3ch = float(c3ch)
-c4g = float(c4g)
-c4ch = float(c4ch)
-c5g = float(c5g)
-c5ch = float(c5ch)
-c6g = float(c6g)
-c6ch = float(c6ch)
-c7g = float(c7g)
-c7ch = float(c7ch)
-
-
-
-
-
-cgpa = int(c1g*c1ch + c2g*c2ch + c3g*c3ch + c4g*c4ch + c5g*c5ch + c6g*c6ch + c7g*c7ch)/(c1ch + c2ch + c3ch + c4ch + c5ch + c6ch + c7ch)
-
-
-
-
-
-
-
-
-print(name ,"your CGPA is :", format(cgpa,".2f") )
+cgpa = "%.2f" % (result/(allCH))
+print(name, "your CGPA is :", cgpa)
